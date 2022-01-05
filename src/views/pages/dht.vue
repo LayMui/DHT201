f08bf167bf9b21e96ab64d33c16ff7940e8b45b1
LayMui <laymui.toh@yara.com>
2022-01-05 19:38:02 +0800

    Add video 1 and 6

--- src/views/pages/dht.vue
+++ src/views/pages/dht.vue
@@ -1,39 +1,51 @@
 <template>
   <div class="text-center">
-   <p class="left">
-      Video 1: <a href="" target="_blank">Video 1
-</a>
-  </p>  
+    <p class="left">
+      Video 1:
+      <a
+        href="https://drive.google.com/file/d/16HNKqXF6fkpKzAtTrIGs0vndxFcf334f/view?usp=sharing"
+        target="_blank"
+        >Video 1
+      </a>
+    </p>
 
- <p class="left">
-      Video 2: <a href="https://drive.google.com/file/d/1t1Mh8EPQt8qyFeK0N3VTV4WST_1JkP3B/view?usp=sharing" target="_blank">Video 2
-</a>
-  </p>  
+    <p class="left">
+      Video 2:
+      <a
+        href="https://drive.google.com/file/d/1t1Mh8EPQt8qyFeK0N3VTV4WST_1JkP3B/view?usp=sharing"
+        target="_blank"
+        >Video 2
+      </a>
+    </p>
 
-   <p class="left">
-      Video 3: <a href="" target="_blank">Video 3
-</a>
-  </p>  
+    <p class="left">Video 3: <a href="" target="_blank">Video 3 </a></p>
 
+    <p class="left">
+      Video 4:
+      <a
+        href="https://drive.google.com/file/d/1FQe9wZAP_sLuM7D0b_2U3OUPtQPzhhgb/view?usp=sharing"
+        target="_blank"
+        >Video 4
+      </a>
+    </p>
+    <p class="left">Video 5: <a href="" target="_blank">Video 5 </a></p>
 
- <p class="left">
-      Video 4: <a href="https://drive.google.com/file/d/1FQe9wZAP_sLuM7D0b_2U3OUPtQPzhhgb/view?usp=sharing" target="_blank">Video 4
-</a>
-  </p>  
-  <p class="left">
-      Video 5: <a href="" target="_blank">Video 5
-</a>
-  </p>  
+    <p class="left">
+      Video 6:
+      <a
+        href="https://drive.google.com/file/d/1s4Gg6QvlglEzlia4z4ofs-V2P3c7216c/view?usp=sharing"
+        target="_blank"
+        >Video 6
+      </a>
+    </p>
 
-   <p class="left">
-      Video 6: <a href="" target="_blank">Video 6
-</a>
-  </p>  
-
-  <p class="left">
-      Video 7: <a href="https://drive.google.com/file/d/1dlBI7iKUbmrraQWiYy6D4M7ItbUwLsbQ/view?usp=sharing" target="_blank">Video 7
-</a>
-  </p>  
-
+    <p class="left">
+      Video 7:
+      <a
+        href="https://drive.google.com/file/d/1dlBI7iKUbmrraQWiYy6D4M7ItbUwLsbQ/view?usp=sharing"
+        target="_blank"
+        >Video 7
+      </a>
+    </p>
   </div>
 </template>
