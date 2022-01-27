dc83545d0cb4a17b7a5254de8f3da34d06299223
LayMui <laymui.toh@yara.com>
2022-01-27 10:53:43 +0800

    Add ack pdf

---
+++ src/views/pages/ack.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center">
+     <p class="left">
+      
+      <a
+        href="https://drive.google.com/file/d/12SylINHShsvpxu244Ve_-4mj5Va6RN-a/view?usp=sharing"
+        style="color:blue;"
+        target="_blank"
+        >Acknowledging what is in you
+      </a>
+    </p>
+     
+
+   </div>
+</template>
--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,6 +1,7 @@
 import video from './video'
 import dht1 from './dht1'
 import dht2 from './dht2'
+import ack from './ack'
 
 const pages = [
   {
@@ -24,6 +25,14 @@ const pages = [
       title: 'DHT Q & A Video 11-19',
     },
     component: dht2,
+  },
+
+  {
+    name: 'Acknowledging what is in you',
+    meta: {
+      title: 'Acknowledging...',
+    },
+    component: ack,
   },
 ]
 
